67f5ddc8fe964fca045962cbab2a068a41efa385
Bobby Mantoni <bobby@mantoni.me>
2020-03-19 20:12:41 +0000

    hex parsing

--- lib/HexLine.dart
+++ lib/HexLine.dart
@@ -6,11 +6,14 @@ class HexComponent {
 
   String get value => _val;
   set value(String val) {
+    print(val);
     if (val.length != _len) {
       throw ArgumentError('Must be ${_len} characters');
     }
     _val = val;
   }
+
+  int get intValue => int.parse(_val, radix: 16);
 }
   
   class HexLine {
@@ -21,5 +24,10 @@ class HexComponent {
   
     HexLine(String line) {
       byteCount.value = line.substring(1, 3);
+      address.value = line.substring(3, 7);
+      recordType.value = line.substring(7, 9);
+      final dataHexCharLen = byteCount.intValue * 2;
+      data = HexComponent(dataHexCharLen)
+        ..value = line.substring(9, 9 + dataHexCharLen);
   }
 }
--- test/hexline_test.dart
+++ test/hexline_test.dart
@@ -4,9 +4,30 @@ import 'package:pic_dart_emu/HexLine.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final lineStr = ':0A0000008316FF2390008312FD2BEE';
+
   test('can set byteCount', () {
-    final lineStr = ':0A0000008316FF2390008312FD2BEE';
     var line = HexLine(lineStr);
     expect(line.byteCount.value, '0A');
+  });
+
+  test('setting wrong length throws exception', () {
+    var line = HexLine(lineStr);
+    expect(() => { line.byteCount.value = '0A2' }, throwsArgumentError);
+  });
+
+  test('parses address', () {
+    var line = HexLine(lineStr);
+    expect(line.address.value, '0000');
+  });
+
+  test('parses recordType', () {
+    var line = HexLine(lineStr);
+    expect(line.recordType.value, '00');
+  });
+
+  test('parses data', () {
+    var line = HexLine(lineStr);
+    expect(line.data.value, '8316FF2390008312FD2B');
   });
 }
